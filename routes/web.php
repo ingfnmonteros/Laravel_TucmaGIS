f06e0b4a5bdcab3210927f773135f90269852b7b
TucmaGIS <ingfnmonteros@gmail.com>
2018-04-05 22:38:57 -0300

    creando UsersController
    php artisan make:controller UsersController
    para listar usurios ... :-)

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/usuarios', 'UsersController@index');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
