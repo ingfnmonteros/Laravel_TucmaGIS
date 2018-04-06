38ff98db91fc588fb2b535f1366fa71ccd85214a
TucmaGIS <ingfnmonteros@gmail.com>
2018-04-06 17:51:46 -0300

    php artisan make:controller UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
-    //
+    public function index() {
+        
+        $usuarios = User::all();
+        
+        return $usuarios;
+    }
 }
